72848a8aa8a196c13c4fde4b062f6dffe3711a83
gianequispe <118861844+gianequispe@users.noreply.github.com>
2024-04-02 16:33:30 -0500

    Now works with keyboard
    now works with keyboard

--- image-carousel/app.js
+++ image-carousel/app.js
@@ -15,3 +15,19 @@ leftBtn.addEventListener("click", () => {
 // mover a la derecha += 600px
 // mover a la izquierda -= 600px
 
+document.onkeydown = function(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            content.scrollLeft -= 550;
+            break;
+        case 'ArrowUp':
+            alert('up');
+            break;
+        case 'ArrowRight':
+            content.scrollLeft += 550;
+            break;
+        case 'ArrowDown':
+            alert('down');
+            break;
+    }
+};
